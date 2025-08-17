2a6114d4da2337aa7d63b9a2469ce22ba9636fb8
Md Shad Khan <devshaad@Mac.lan>
2025-08-17 16:20:09 +0530

    Feat: added haircut explorer route

--- src/app/dashboard/haircut/page.tsx
+++ src/app/dashboard/haircut/page.tsx
@@ -1,12 +1,11 @@
 import Footer from "@/components/layout/Footer";
+import Haircut from "@/components/layout/haircut/Haircut";
 import React from "react";
 
 function Page() {
   return (
     <>
-      <div className="min-h-screen w-full flex justify-center items-center bg-red-50">
-        503 | Under Construction
-      </div>
+      <Haircut />
       <Footer />
     </>
   );
