e2db1f614aaa5f8eda671d8f2a06d4163a135e7a
Md Shad Khan <devshaad@Mac.lan>
2025-08-28 22:05:42 +0530

    Feat: added canceled queue option to see the canceled queues

--- src/components/layout/booking/AllBooking.tsx
+++ src/components/layout/booking/AllBooking.tsx
@@ -14,6 +14,8 @@ import {
 import { Separator } from "../../ui/separator";
 import { formatWaitTime } from "@/hooks/useFormatWaitTime";
 import { handleQueueDone } from "@/hooks/useCancelBooking";
+import { useGetCancelledBooking } from "@/hooks/useGetCancelledBooking";
+import { Loader2 } from "lucide-react";
 
 const InQueue = ({
   userId,
@@ -24,13 +26,19 @@ const InQueue = ({
 }) => {
   const userQueue = useUserQueue(userId);
 
-  if (!userQueue || userQueue.length === 0) {
-    return (
-      <>
-        <p className="text-center">No bookings found</p>
-      </>
-    );
-  }
+  const { cancelledQueues, loading } = useGetCancelledBooking(userId);
+
+  const handleGetCancelledBooking = () => {
+    console.log(cancelledQueues);
+  };
+
+  // if (!userQueue || userQueue.length === 0) {
+  //   return (
+  //     <>
+  //       <p className="text-center">No bookings found</p>
+  //     </>
+  //   );
+  // }
 
   if (type !== "upcoming") {
     //Todo: fix it to production level when the things are done
@@ -42,84 +50,116 @@ const InQueue = ({
       );
     } else if (type === "cancelled") {
       return (
-        <>
-          <p className="text-center">No cancelled bookings found</p>
-        </>
+        <div className="space-y-4">
+          {loading ? (
+            <LoadingSpinner />
+          ) : cancelledQueues.length > 0 ? (
+            cancelledQueues.map((data) => (
+              <Card
+                className="w-full border border-yellow-700 bg-yellow-50"
+                key={data.id}
+              >
+                <CardContent className="">
+                  <h2 className="text-lg font-semibold">
+                    {data.barber_shop_name}
+                  </h2>
+                  <p className="text-sm text-gray-600 font-semibold">
+                    Canceled at: {data.canceledAt.toDate().toLocaleString()}
+                  </p>
+                </CardContent>
+              </Card>
+            ))
+          ) : (
+            <p className="text-center">No cancelled bookings found</p>
+          )}
+        </div>
       );
     }
   }
 
   return (
     <div className="space-y-4">
-      {userQueue.map((data) => (
-        <Card
-          className="w-full border border-yellow-700 bg-yellow-50"
-          key={data.barber_shop.id}
-        >
-          <CardContent className="">
-            <h2 className="text-lg font-semibold">{data.barber_shop.name}</h2>
-            <p className="text-sm text-gray-600 font-semibold">
-              #{data.queue_details.queue_position} •{" "}
-              {data.queue_details.cutType} •{" "}
-              {data.queue_details.estTime > 0
-                ? formatWaitTime(Number(data.queue_details.estTime))
-                : "It's your turn"}
-            </p>
-            <div className="text-sm space-x-2 text-yellow-700 mt-2">
-              <Drawer>
-                <DrawerTrigger>
-                  <span className="underline underline-offset-2">
-                    Cancel Booking
-                  </span>
-                </DrawerTrigger>
-                <DrawerContent
-                  className="pb-6 z-[9999]"
-                  overlayClassName="backdrop-blur-md bg-transparent"
-                >
-                  <DrawerHeader>
-                    <DrawerTitle className="text-yellow-800">
+      {userQueue.length > 0 ? (
+        userQueue.map((data) => (
+          <Card
+            className="w-full border border-yellow-700 bg-yellow-50"
+            key={data.barber_shop.id}
+          >
+            <CardContent className="">
+              <h2 className="text-lg font-semibold">{data.barber_shop.name}</h2>
+              <p className="text-sm text-gray-600 font-semibold">
+                #{data.queue_details.queue_position} •{" "}
+                {data.queue_details.cutType} •{" "}
+                {data.queue_details.estTime > 0
+                  ? formatWaitTime(Number(data.queue_details.estTime))
+                  : "It's your turn"}
+              </p>
+              <div className="text-sm space-x-2 text-yellow-700 mt-2">
+                <Drawer>
+                  <DrawerTrigger>
+                    <span className="underline underline-offset-2">
                       Cancel Booking
-                    </DrawerTitle>
-                    <Separator className="my-4" />
-                    <DrawerDescription className="flex flex-col">
-                      <span className="text-base font-semibold text-[#363636]">
-                        Are you sure want to cancel your booking?
-                      </span>
-                      <span className="font-light mt-1">
-                        Your spot will be released, but you can schedule a new
-                        appointment anytime.
-                      </span>
-                    </DrawerDescription>
-                  </DrawerHeader>
-                  <DrawerFooter className="flex items-center flex-row justify-between">
-                    <DrawerClose asChild>
-                      <span className="px-4 flex rounded-full justify-center items-center py-2 w-full border">
-                        Cancel
-                      </span>
-                    </DrawerClose>
-                    <span
-                      className="px-4 w-full py-2 flex rounded-full justify-center items-center border bg-yellow-700 hover:bg-yellow-800 text-white"
-                      onClick={() =>
-                        handleQueueDone({
-                          shopId: data.barber_shop.id,
-                          userId: userId,
-                        })
-                      }
-                    >
-                      Confirm
                     </span>
-                  </DrawerFooter>
-                </DrawerContent>
-              </Drawer>
-              <span className="underline underline-offset-2">
-                View E-Receipt
-              </span>
-            </div>
-          </CardContent>
-        </Card>
-      ))}
+                  </DrawerTrigger>
+                  <DrawerContent
+                    className="pb-6 z-[9999]"
+                    overlayClassName="backdrop-blur-md bg-transparent"
+                  >
+                    <DrawerHeader>
+                      <DrawerTitle className="text-yellow-800">
+                        Cancel Booking
+                      </DrawerTitle>
+                      <Separator className="my-4" />
+                      <DrawerDescription className="flex flex-col">
+                        <span className="text-base font-semibold text-[#363636]">
+                          Are you sure want to cancel your booking?
+                        </span>
+                        <span className="font-light mt-1">
+                          Your spot will be released, but you can schedule a new
+                          appointment anytime.
+                        </span>
+                      </DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter className="flex items-center flex-row justify-between">
+                      <DrawerClose asChild>
+                        <span className="px-4 flex rounded-full justify-center items-center py-2 w-full border">
+                          Cancel
+                        </span>
+                      </DrawerClose>
+                      <span
+                        className="px-4 w-full py-2 flex rounded-full justify-center items-center border bg-yellow-700 hover:bg-yellow-800 text-white"
+                        onClick={() =>
+                          handleQueueDone({
+                            shopId: data.barber_shop.id,
+                            userId: userId,
+                          })
+                        }
+                      >
+                        Confirm
+                      </span>
+                    </DrawerFooter>
+                  </DrawerContent>
+                </Drawer>
+                <span className="underline underline-offset-2">
+                  View E-Receipt
+                </span>
+              </div>
+            </CardContent>
+          </Card>
+        ))
+      ) : (
+        <p className="text-center">No upcoming bookings found.</p>
+      )}
     </div>
   );
 };
 
 export default InQueue;
+
+const LoadingSpinner = () => {
+  return (
+    <div className="mt-10 flex flex-col justify-center items-center">
+      <Loader2 className="animate-spin" />
+    </div>
+  );
+};
--- src/hooks/useCancelBooking.ts
+++ src/hooks/useCancelBooking.ts
@@ -1,7 +1,14 @@
 import { db } from "@/config/firebase.config";
-import { deleteDoc, doc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  setDoc,
+} from "firebase/firestore";
 
-export const handleQueueDone = ({
+export const handleQueueDone = async ({
   shopId,
   userId,
 }: {
@@ -9,12 +16,33 @@ export const handleQueueDone = ({
   userId: string | null | undefined;
 }) => {
   if (!shopId || !userId) return;
-  const ref = doc(db, "shops", shopId, "queues", userId);
-  deleteDoc(ref)
-    .then(() => {
-      console.log("Cutting Done");
-    })
-    .catch((err) => {
-      console.error(err.message);
-    });
+  try {
+    const queueRef = doc(db, "shops", shopId, "queues", userId);
+    const canceledRef = collection(db, "shops", shopId, "canceledQueues");
+
+    // Get the current queue data
+    const snap = await getDoc(queueRef);
+    const barber_shop_id = snap.data()?.barberUserId;
+
+    const barberRef = doc(db, "shops", barber_shop_id);
+    const barberSnap = await getDoc(barberRef);
+    const barber_shop_name = barberSnap.data()?.name;
+
+    if (snap.exists()) {
+      const data = snap.data();
+
+      // Save it to canceledQueues
+      await addDoc(canceledRef, {
+        ...data,
+        userId: userId,
+        barber_shop_name: barber_shop_name,
+        canceledAt: new Date(),
+      });
+
+      // Delete from active queue
+      await deleteDoc(queueRef);
+    }
+  } catch (err: any) {
+    console.error("Error moving queue to canceled:", err.message);
+  }
 };
